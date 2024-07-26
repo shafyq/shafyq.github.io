d3b09d796fbcd19672ebc040565b58e933a07576
Shafiq <176507586+shafyq@users.noreply.github.com>
2024-07-26 18:24:32 +0600

    Update script.js

--- script.js
+++ script.js
@@ -11,31 +11,36 @@ document.addEventListener('DOMContentLoaded', () => {
         .then(data => {
             document.querySelector('#isp span').textContent = data.org;
             document.querySelector('#location span').textContent = `${data.city}, ${data.country_name}`;
-            updateTimeAndDate(data.timezone);
+            startClock(data.timezone);
         })
         .catch(error => {
             console.error('Error fetching data:', error);
         });
 
-    function updateTimeAndDate(timezone) {
-        const now = new Date();
-        const options = {
-            timeZone: timezone,
-            hour: '2-digit',
-            minute: '2-digit',
-            second: '2-digit',
-            hour12: false
-        };
-        const timeString = now.toLocaleTimeString([], options);
-        document.querySelector('#time span').textContent = timeString;
+    function startClock(timezone) {
+        function updateTimeAndDate() {
+            const now = new Date();
+            const options = {
+                timeZone: timezone,
+                hour: '2-digit',
+                minute: '2-digit',
+                second: '2-digit',
+                hour12: false
+            };
+            const timeString = now.toLocaleTimeString([], options);
+            document.querySelector('#time span').textContent = timeString;
 
-        const dateOptions = {
-            timeZone: timezone,
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric'
-        };
-        const dateString = now.toLocaleDateString([], dateOptions);
-        document.querySelector('#date span').textContent = dateString;
+            const dateOptions = {
+                timeZone: timezone,
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+            };
+            const dateString = now.toLocaleDateString([], dateOptions);
+            document.querySelector('#date span').textContent = dateString;
+        }
+
+        updateTimeAndDate();
+        setInterval(updateTimeAndDate, 1000);
     }
 });
