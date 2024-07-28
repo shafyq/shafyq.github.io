632031307df2aa550a1500fb2df349c05d8fdb84
Shafiq <176507586+shafyq@users.noreply.github.com>
2024-07-29 00:24:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const ipAddressElement = document.getElementById("ip-address");
+    const cityElement = document.getElementById("city");
+    const ispElement = document.getElementById("isp");
 
     // Replace 'YOUR_API_KEY' with your actual ipinfo.io API key
     const apiKey = "00fbc71f8f38cc";
@@ -8,10 +10,14 @@ document.addEventListener("DOMContentLoaded", function() {
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            ipAddressElement.textContent = data.ip;
+            ipAddressElement.textContent = `IP Address: ${data.ip}`;
+            cityElement.textContent = `City: ${data.city}`;
+            ispElement.textContent = `ISP: ${data.org}`;
         })
         .catch(error => {
             console.error("Error fetching IP address:", error);
             ipAddressElement.textContent = "Unable to fetch IP address.";
+            cityElement.textContent = "";
+            ispElement.textContent = "";
         });
 });
